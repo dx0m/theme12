e09c564ee8ca71cccc553686905f03e9b7ad5ee4
vitaliy <vorob2008@yandex.ru>
2022-12-20 17:08:21 +0300

    ver 0.8

--- app.py
+++ app.py
@@ -2,19 +2,24 @@ from flask import Flask, request, redirect, url_for, session
 
 app = Flask(__name__)
 app.secret_key = 'abc'
+mess = [None,None,None,None]
 
 @app.route('/', methods=['GET', 'POST'])
 def authorization():
-    mess = []
+    global mess
     while request.method == 'POST':
        mess.append(request.form.get('Message'))
        return '''
              <form method="POST">
                  <div><label>Message: <input type="text" name="Message"></label></div>
                  <input type="submit" value="Enter">
+                 <div><h1>Четыре последних сообщения:</h1></div>
+                 <div>{}</div>
+                 <div>{}</div>
+                 <div>{}</div>
                  <div>{}</div>
              </form>
-             '''.format(mess[0])
+             '''.format(mess[-1],mess[-2],mess[-3],mess[-4])
 
     return '''
              <form method="POST">
